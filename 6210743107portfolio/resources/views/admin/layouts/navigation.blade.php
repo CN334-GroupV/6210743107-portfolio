3e03624ae43cc60cec0e9beeb27cd4616d668da9
tananyapat <tanakorntananyapat@gmail.com>
2023-01-23 13:28:43 +0700

    *-*

--- 6210743107portfolio/resources/views/admin/layouts/navigation.blade.php
+++ 6210743107portfolio/resources/views/admin/layouts/navigation.blade.php
@@ -16,11 +16,11 @@
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                <!-- <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="url('posts')" :active="request()->routeIs('dashboard')">
                         {{ __('Project') }}
                     </x-nav-link>
-                </div>
+                </div> -->
             </div>
 
             <!-- Settings Dropdown -->
